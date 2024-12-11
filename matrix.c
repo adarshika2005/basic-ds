f9751ca40bf65a586554339883756d06ad6e37e4
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-12-11 22:09:51 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main() {
+    int a[4][4], b[4][4], c[4][4], n, i, j, k;
+
+    printf("Enter the value of N (N <= 4): ");
+    scanf("%d", & n);
+    printf("Enter the elements of Matrix-A: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", & a[i][j]);
+        }
+    }
+    printf("Enter the elements of Matrix-B: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", & b[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            c[i][j] = 0;
+            for (k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    printf("The product of the two matrices is: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+2
